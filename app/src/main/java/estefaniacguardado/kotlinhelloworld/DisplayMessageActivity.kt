5181936de745d2bd2d1720fe1467a34b3482d424
Estefania Guardado <fachinacg@gmail.com>
2018-02-23 15:20:42 +0100

    Init Table

--- app/src/main/java/estefaniacguardado/kotlinhelloworld/DisplayMessageActivity.kt
+++ app/src/main/java/estefaniacguardado/kotlinhelloworld/DisplayMessageActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 
 
@@ -19,5 +22,15 @@ class DisplayMessageActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = message
+
+        val tableLayout = findViewById<View>(R.id.mainTable) as TableLayout
+        val row = TableRow(this)
+        val content = TextView(this)
+        content.setText(textView.text)
+        row.addView(content)
+
+
+        tableLayout.addView(row)
+
     }
 }
